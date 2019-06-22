797253a69d2b1044f706f3ce7f7d42c3104f3214
nickl0gist <nicklogistick@mail.ru>
2019-06-22 10:34:19 +0200

    ID_Validator finished. Tests passed

--- src/main/java/ID_Validator.java
+++ src/main/java/ID_Validator.java
@@ -5,7 +5,8 @@ public class ID_Validator implements Validator {
 
     String type = "ID number";
 
-    Map<Character, Integer> signMap = new HashMap<>();
+    private Map<Character, Integer> signMap = new HashMap<>();
+    private int[] points = {7, 3, 1, 9, 7, 3, 1, 7, 3};
 
     public ID_Validator() {
         this.signMap.put('A', 10); this.signMap.put('N', 23);
@@ -21,10 +22,25 @@ public class ID_Validator implements Validator {
         this.signMap.put('K', 20); this.signMap.put('X', 33);
         this.signMap.put('L', 21); this.signMap.put('Y', 34);
         this.signMap.put('M', 22); this.signMap.put('Z', 35);
+
+
     }
 
     @Override
     public boolean validate(String id_number) {
-        return false;
+        int control_sum=0;
+
+        id_number = id_number.replaceAll("[- ]", "").toUpperCase();
+        if(id_number.length() != points.length || !id_number.matches("^[A-Z]{3}[0-9]{6}$")) return false;
+
+        for (int i = 0; i < id_number.length(); i++) {
+            if (i < 3) {
+                control_sum += signMap.get(id_number.charAt(i)) * points[i];
+            } else {
+                control_sum += Character.getNumericValue(id_number.charAt(i)) * points[i];
+            }
+        }
+
+        return control_sum % 10 == 0;
     }
 }
