175273b837aa4a986fb17d59570a777bcef00466
nickl0gist <nicklogistick@mail.ru>
2019-06-22 11:12:26 +0200

    Ctrl+Alt+L

--- src/main/java/PeselValidator.java
+++ src/main/java/PeselValidator.java
@@ -6,18 +6,17 @@ public class PeselValidator implements Validator {
      * cyfry [7-10] – numer serii z oznaczeniem płci
      * cyfra [10] – płeć
      * cyfra [11] – cyfra kontrolna
-     *
      */
 
     private String type = "PESEL number";
 
-    private int[] points = {9,7,3,1,9,7,3,1,9,7};
+    private int[] points = {9, 7, 3, 1, 9, 7, 3, 1, 9, 7};
 
     @Override
     public boolean validate(String id_number) {
         int control_sum = 0;
         id_number = id_number.replaceAll("[- ]", "");
-        if(id_number.length() != points.length + 1 || !id_number.matches("^[0-9]{11}$")) return false;
+        if (id_number.length() != points.length + 1 || !id_number.matches("^[0-9]{11}$")) return false;
         for (int i = 0; i < points.length; i++) {
             control_sum += points[i] * Character.getNumericValue(id_number.charAt(i));
         }
--- src/test/java/ID_ValidatorTest.java
+++ src/test/java/ID_ValidatorTest.java
@@ -8,57 +8,57 @@ public class ID_ValidatorTest {
     private ID_Validator validator;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         this.validator = new ID_Validator();
     }
 
     @Test
-    public void ValidID_test(){
+    public void ValidID_test() {
         assertTrue(validator.validate("NRX300202"));
     }
 
     @Test
-    public void ValidID_with_space_test(){
+    public void ValidID_with_space_test() {
         assertTrue(validator.validate("NRX 300202"));
     }
 
     @Test
-    public void ValidID_with_dash_test(){
+    public void ValidID_with_dash_test() {
         assertTrue(validator.validate("NRX-300202"));
     }
 
     @Test
-    public void ValidID_case_sensitive_test(){
+    public void ValidID_case_sensitive_test() {
         assertTrue(validator.validate("Nrx 300202"));
     }
 
     @Test
-    public void InValidID_test(){
+    public void InValidID_test() {
         assertFalse(validator.validate("NRX222333"));
     }
 
     @Test
-    public void InValidID_with_space_test(){
+    public void InValidID_with_space_test() {
         assertFalse(validator.validate("NRX 222333"));
     }
 
     @Test
-    public void InValidID_with_dash_test(){
+    public void InValidID_with_dash_test() {
         assertFalse(validator.validate("NRX-222333"));
     }
 
     @Test
-    public void InValidID_case_sensitive_test(){
+    public void InValidID_case_sensitive_test() {
         assertFalse(validator.validate("Nrx222333"));
     }
 
     @Test
-    public void InValidID_regex_test(){
+    public void InValidID_regex_test() {
         assertFalse(validator.validate("nR5 300202"));
     }
 
     @Test
-    public void ValidID_regex_test(){
+    public void ValidID_regex_test() {
         assertTrue(validator.validate("NRX 300202"));
     }
 }
